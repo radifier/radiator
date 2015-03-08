88176b17a9094bec8e437a7761117ac6faf5121b
KlausT <kto@gmx.de>
2015-03-08 16:59:08 +0100

    fix intensity option for whirlpoolx

--- x15/whirlpoolx.cu
+++ x15/whirlpoolx.cu
@@ -45,10 +45,12 @@ int scanhash_whirlpoolx(int thr_id, uint32_t *pdata, uint32_t *ptarget, uint32_t
 {
 	const uint32_t first_nonce = pdata[19];
 	uint32_t endiandata[20];
-	uint32_t throughput = 1 << 26;
+
+	uint32_t throughput = device_intensity(thr_id, __func__, 1 << 26); // 256*4096
+	throughput = min(throughput, max_nonce - first_nonce);
 
 	if (opt_benchmark)
-		ptarget[7] = 0x03;
+		ptarget[7] = 0x0f;
 
 	if (!init[thr_id]) {
 
