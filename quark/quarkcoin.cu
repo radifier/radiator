ff2ac930346cd4993b6155598860202bada3c88b
sp-hash <rune.stensland@bradyplc.com>
2015-08-22 08:53:22 +0200

    Fix quark hashrate on the pool

--- quark/quarkcoin.cu
+++ quark/quarkcoin.cu
@@ -223,11 +223,11 @@ extern int scanhash_quark(int thr_id, uint32_t *pdata,
 
 		// quarkNonces in branch1 und branch2 aufsplitten gemäss if (hash[0] & 0x8)
 		quark_compactTest_cpu_hash_64(thr_id, nrm3, pdata[19], d_hash[thr_id], d_branch3Nonces[thr_id],
-			d_branch3Nonces[thr_id], &nrm1,
-			d_branch2Nonces[thr_id], &nrm2);
+			d_branch1Nonces[thr_id], &nrm1,
+			d_branch3Nonces[thr_id], &nrm2);
 
-		quark_keccak512_cpu_hash_64_final(thr_id, nrm1, pdata[19], d_branch3Nonces[thr_id], d_hash[thr_id]);
-		quark_jh512_cpu_hash_64_final(thr_id, nrm2, pdata[19], d_branch2Nonces[thr_id], d_hash[thr_id]);
+		quark_keccak512_cpu_hash_64_final(thr_id, nrm1, pdata[19], d_branch1Nonces[thr_id], d_hash[thr_id]);
+		quark_jh512_cpu_hash_64_final(thr_id, nrm2, pdata[19], d_branch3Nonces[thr_id], d_hash[thr_id]);
 		
 		cuda_check_quarkcoin(thr_id, nrm3, pdata[19], d_branch3Nonces[thr_id], d_hash[thr_id], foundnonces);
 		if(stop_mining) {mining_has_stopped[thr_id] = true; cudaStreamDestroy(gpustream[thr_id]); pthread_exit(nullptr);}
