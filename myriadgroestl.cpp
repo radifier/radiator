858bbf6deb3bd2fb91a6c1ba5c101ca72900ff04
KlausT <kto@gmx.de>
2015-10-20 22:53:08 +0200

    fix hashes_done for myr-gr

--- myriadgroestl.cpp
+++ myriadgroestl.cpp
@@ -44,8 +44,8 @@ extern int scanhash_myriad(int thr_id, uint32_t *pdata, uint32_t *ptarget,
 {
 	static THREAD uint32_t *h_found = nullptr;
 
-	uint32_t start_nonce = pdata[19]++;
-	uint32_t throughput = device_intensity(device_map[thr_id], __func__, 1 << 20);
+	uint32_t start_nonce = pdata[19];
+	uint32_t throughput = device_intensity(device_map[thr_id], __func__, 1 << 19);
 	throughput = min(throughput, max_nonce - start_nonce) & 0xfffffc00;
 
 	if (opt_benchmark)
@@ -59,7 +59,7 @@ extern int scanhash_myriad(int thr_id, uint32_t *pdata, uint32_t *ptarget,
 #else
 		myriadgroestl_cpu_init(thr_id, throughput);
 #endif
-		cudaMallocHost(&(h_found), 4 * sizeof(uint32_t));
+		cudaMallocHost(&h_found, 4 * sizeof(uint32_t));
 		init = true;
 	}
 
@@ -130,7 +130,7 @@ extern int scanhash_myriad(int thr_id, uint32_t *pdata, uint32_t *ptarget,
 		}
 	} while (!work_restart[thr_id].restart && ((uint64_t)max_nonce > ((uint64_t)(pdata[19]) + (uint64_t)throughput)));
 
-	*hashes_done = pdata[19] - start_nonce + 1;
+	*hashes_done = pdata[19] - start_nonce;
 	return 0;
 }
 
