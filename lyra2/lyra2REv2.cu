325a377159d2d5cbe06c380cc4f5dfff0d937a8b
KlausT <kto@gmx.de>
2016-06-17 08:07:00 +0200

    lyra2v2: higher default intensity for 64bit builds

--- lyra2/lyra2REv2.cu
+++ lyra2/lyra2REv2.cu
@@ -87,18 +87,30 @@ int scanhash_lyra2v2(int thr_id, uint32_t *pdata,
 	if(strstr(props.name, "1080"))
 	{
 		intensity = 256 * 256 * 15;
+#ifdef _WIN64
+		intensity = 256 * 256 * 22;
+#endif
 	}
 	else if(strstr(props.name, "1070"))
 	{
 		intensity = 256 * 256 * 15;
+#ifdef _WIN64
+		intensity = 256 * 256 * 22;
+#endif
 	}
 	else if(strstr(props.name, "970"))
 	{
 		intensity = 256 * 256 * 15;
+#ifdef _WIN64
+		intensity = 256 * 256 * 22;
+#endif
 	}
 	else if (strstr(props.name, "980"))
 	{
+		intensity = 256 * 256 * 22;
+#ifdef _WIN64
 		intensity = 256 * 256 * 15;
+#endif
 	}
 	else if (strstr(props.name, "750 Ti"))
 	{
