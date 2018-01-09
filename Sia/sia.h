a7a7e229112d19d7bdbf1bdf7750b8b46242ded2
KlausT <kto@gmx.de>
2018-01-09 17:07:55 +0100

    add GPU number to the CUDA error message
    (cherry picked from commit e094b5b97531da51641693fcafa39deca0daa5ef)

--- Sia/sia.h
+++ Sia/sia.h
@@ -6,5 +6,5 @@
 #define blocksize 512
 
 void sia_gpu_init(int thr_id);
-void sia_precalc(cudaStream_t cudastream, const uint64_t *blockHeader);
+void sia_precalc(int thr_id, cudaStream_t cudastream, const uint64_t *blockHeader);
 void sia_gpu_hash(cudaStream_t cudastream, int thr_id, uint32_t threads, uint32_t *nonceOut, uint64_t target, uint64_t startnonce);
