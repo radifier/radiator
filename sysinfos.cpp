39440761391dd51c8eaa5253e10e55ac72301c5c
KlausT <kto@gmx.de>
2018-01-28 15:57:59 +0100

    fix linux_cpufreq()

--- sysinfos.cpp
+++ sysinfos.cpp
@@ -39,17 +39,26 @@ static double linux_cputemp(int core)
 	return tc;
 }
 
-#define CPUFREQ_PATH \
- "/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq"
 static uint32_t linux_cpufreq(int core)
 {
-	FILE *fd = fopen(CPUFREQ_PATH, "r");
 	uint32_t freq = 0;
+	FILE *fd;
 
-	if(!fd)
+	fd = fopen("/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq", "r");
+	if(fd != NULL)
 	{
 		fscanf(fd, "%d", &freq);
 		fclose(fd);
+	}
+	else // sometimes we don't have read permission for cpuinfo_cur_freq
+	{
+		fd = fopen("/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq", "r");
+		if(fd != NULL)
+		{
+			fscanf(fd, "%d", &freq);
+			fclose(fd);
+		}
+
 	}
 	return freq;
 }
